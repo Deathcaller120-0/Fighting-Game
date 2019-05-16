b051afa363abf6ec6e89946f1b21112457ce2482
Deathcaller120-0 <49953301+Deathcaller120-0@users.noreply.github.com>
2019-05-16 13:22:01 -0700

    Update js.js

--- js.js
+++ js.js
@@ -71,7 +71,7 @@ function live(){
 	if (player1.live !== "Alive"){
 		player1.live = "Loading, Please Wait";
 		var rngLvl = Math.floor(Math.random() * player0.lvl + 1);
-		var rngHP = Math.floor(Math.random() * player0.HP + 2);
+		var rngHP = Math.floor(Math.random() * maxhp + 2);
 		var rngH0 = Math.floor(Math.random() * weaponlist.length) - player0.HP;
 		var rngH1 = Math.floor(Math.random() * weaponlist.length) - player0.HP;
 		var rngED = Math.floor(Math.random() * player0.lvl ** 2 + 20);
